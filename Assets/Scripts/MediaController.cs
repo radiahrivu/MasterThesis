6f669afe9916d94b752fe383e20aaecb38032509
jryisme <jryisme@gmail.com>
2020-01-10 16:07:44 +0100

    the first commit

---
+++ Assets/Scripts/MediaController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediaController : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.playOnAwake = false;
+    }
+
+    public void OnClickPlayVideo()
+    {
+
+    }
+}
