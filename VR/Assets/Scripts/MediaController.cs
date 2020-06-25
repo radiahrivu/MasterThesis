aed0f3c3f714fa718ef3f19521d90752b9367bd3
jryisme <jryisme@gmail.com>
2020-06-25 19:00:14 +0200

    More experiments

--- VR/Assets/Scripts/MediaController.cs
+++ VR/Assets/Scripts/MediaController.cs
@@ -153,7 +153,7 @@ public class MediaController : MonoBehaviour
         //connString = "URI=file:" + Application.dataPath + "/DB/Test.sqlite";
 
         // to change
-        userId = 16;
+        userId = 3;
 
         setting = new ExperimentSetting();
         setting = setting.GetExperimentSettingByUserId(connString, userId);
