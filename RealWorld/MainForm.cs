78d4b86e9b07efba8bb56dfcdc82ea1ad3cbe219
jryisme <jryisme@gmail.com>
2020-07-23 15:40:49 +0200

    More experiments

--- RealWorld/MainForm.cs
+++ RealWorld/MainForm.cs
@@ -80,7 +80,7 @@ namespace RealWorld
             //connString = "URI=file:C:/Projects/Ruoyu MT/MasterThesis/VR/Assets/DB" + "/Production.sqlite";
 
             // to change
-            userId = 21;
+            userId = 24;
 
             setting = new ExperimentSetting();
             setting = setting.GetExperimentSettingByUserId(connString, userId);
--- VR/Assets/Scripts/MediaController.cs
+++ VR/Assets/Scripts/MediaController.cs
@@ -153,7 +153,7 @@ public class MediaController : MonoBehaviour
         //connString = "URI=file:" + Application.dataPath + "/DB/Test.sqlite";
 
         // to change
-        userId = 22;
+        userId = 23;
 
         setting = new ExperimentSetting();
         setting = setting.GetExperimentSettingByUserId(connString, userId);
